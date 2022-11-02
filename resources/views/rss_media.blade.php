b47157e63c46e1c9c90e5122e14ee9aa12baff5d
islamshu12 <islamshu12@gmail.com>
2022-11-02 21:02:37 +0200

    edit

--- resources/views/rss_media.blade.php
+++ resources/views/rss_media.blade.php
@@ -106,17 +106,17 @@
     <div class="container">
         <div class="podcast-header">
             <h1>
-                {{ dd($flux->channel) }}
                 <div class="podcast-image">
                     <a href="{{ (string)$flux->channel->link }}"><img src="{{ (string)$flux->channel->image->url }}" title="{{ (string)$flux->channel->title }}"></a>
-                </div>
-                
-                 </h1>
-            <p>الجمهور المتفاعل هو بداية نجاح اي حملة تسويقية ، اتكلم عن بعض الاسباب لفشل الحملات التسويقية يلا نستمتع …</p>
+                </div>  {{ (string)$flux->channel->title }}   </h1>
+            <p>
+                {{ (string)$flux->channel->description }} 
+            </p>
             <p><a href="{{ (string)$flux->channel->link }}" target="_blank">
                 Visit podcast website →
               </a></p>
         </div>
+        @foreach ($$flux->channel->item as $item)
         <div class="item">
             <h2><a href="https://podeo.co" target="_blank">تعلم التسويق قبل إنشاء الحملة التسويقية</a></h2>
             <div class="episode-time"><span>Fri, 28 Oct 2022 11:00:00 +0000</span> •
@@ -125,107 +125,10 @@
                 حوار بين مجموعة من المسوقين يتحدثوا عن أهمية دراسة التسويق وأساسياته، وأن التسويق ليست مقتصرة على الحملة التسويقية وليست مقتصرة على منصات التواصل الاجتماعي، بل التسويق كيف أحصل على ولاء العميل؟ وأمثلة على الاخفاقات التسويقية ونصائح لتحديد جمهورك المستهدف
                 وكيفية تثقيف الجمهور بمنتجك أو خدمتك
             </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/NzAzMA/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">مع شادي حماد JRS أسرار نجاح جروب</a></h2>
-            <div class="episode-time"><span>Fri, 14 Oct 2022 11:00:00 +0000</span> •
-                <span>53 minutes</span></div>
-            <p>
-                Jordanian Real Sales - JRS حوار مع صانع المحتوى ومؤسس جروب، شادي حماد تحدثنا عن أهمية صناعة المحتوى على الفيسبوك وسر التسويق الفيروسي في برنامج سهول سحاب
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/NjcyMQ/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">فضفضة مسوقين_قطاع الأعمال</a></h2>
-            <div class="episode-time"><span>Fri, 07 Oct 2022 11:00:00 +0000</span> •
-                <span>44 minutes</span></div>
-            <p>
-                حلقة مميزة مع المسوقة فاطمة مهدي صاحبة الخبرة العالية في التسويق، تحكي قصة دخولها في المجال وكيفية تطوير موهبتها وتشارك خبراتها ونصائح للمسوقين في كيفية فهم العميل ودراسة نفسيته .. وتحديات الوصول إلى العملاء واستخدام المحتوى التسويقي لاستهداف قطاع الأعمال
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/NjU0Mw/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">كيف تسوق لعطرك؟</a></h2>
-            <div class="episode-time"><span>Sat, 01 Oct 2022 06:00:00 +0000</span> •
-                <span>18 minutes</span></div>
-            <p>
-                من إحدى مساحات تويتر .. سؤال إحدى المشاركات كيف تسوق لعطرها؟ والإجابة كانت بعدة طرق وإستراتيجيات ومن مؤثرين على السوشيال ميديا ومن خلال هذه الحلقة ستستفيد كيف تسوق لعطرك بشكل أفضل وكيف تصل إلى العملاء ..
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/NjQ5Mg/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">مسوقة تبحث عن حل لمشكلة</a></h2>
-            <div class="episode-time"><span>Sun, 25 Sep 2022 15:07:54 +0000</span> •
-                <span>26 minutes</span></div>
-            <p>
-                مسوقة تبحث عن حل لمشكلة
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/NjM5Nw/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">من مساحات تويتر | صناعة المحتوى مع زيد سلامة ومحمد الغندور</a></h2>
-            <div class="episode-time"><span>Mon, 12 Sep 2022 13:42:44 +0000</span> •
-                <span>69 minutes</span></div>
-            <p>
-                ملخص مساحة عبر تويتر بعنوان صناعة المحتوى، تتضمن حوار بين خبير التسويق وصناعة المحتوى محمد الغندور والمؤثر التقني زيد سلامة يتحدثان عن أهم مصادر الدخل من صناعة المحتوى، وكيفية تحديد فئتك المستهدفة وكيفية التخطيط للمحتوى وأفكار لتوليد محتوى ونصائح لصناع
-                المحتوى وكلمات تحفيزية لمقاومة الملل ومحطات اليأس الذي يمر بها كل صانع محتوى.
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/NjAwNA/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">صناعة المحتوى مع رند المجالي و محمد الغندور</a></h2>
-            <div class="episode-time"><span>Sat, 10 Sep 2022 15:21:59 +0000</span> •
-                <span>69 minutes</span></div>
-            <p>
-                تحدثنا في هذا البودكاست مع الرائعة صانعة المحتوى رند مجالي عن النقاط المهمة التي يجب ان تتوفر عن كل صانع محتوى ومشاركة بعض التجارب العملية من رند المجالي و محمد الغندور
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/MzcxMQ/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">طرق الدخل السلبي من التجارة الالكترونية</a></h2>
-            <div class="episode-time"><span>Sun, 31 Jul 2022 09:20:00 +0000</span> •
-                <span>19 minutes</span></div>
-            <p>
-                انا حابك تفكر وحاب انك تعرف من اين تأكل الكتف و في عبارات دائما بذكرها وحاب انك تضعها في حسبانك كيف انا ممكن أوصل لأكبر عدد من الجمهور كيف اعرف أي مجال اتخصص فيه وبناء عليه تقرر
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/MzIyOQ/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">الدخل السلبي</a></h2>
-            <div class="episode-time"><span>Fri, 22 Jul 2022 11:00:00 +0000</span> •
-                <span>11 minutes</span></div>
-            <p>
-                انا كيف ممكن يكون عندي مصادر دخل أخرى من أصول رقمية او من محتوى او من حسابات على وسائل التواصل الاجتماعي او من التجارة الالكترونية راح نتكلم عن الموضوع بشكل بسيط عن الدخل السلبي
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/MzEwMA/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">المدفوعات الالكترونية</a></h2>
-            <div class="episode-time"><span>Thu, 14 Jul 2022 11:00:00 +0000</span> •
-                <span>26 minutes</span></div>
-            <p>
-                من النقاط المهمة و التي يجب توفيرها في موقعك الإلكتروني وهي المدفوعات الالكترونية
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/Mjk2NQ/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">أخطاء شائعة في التجارة الإلكترونية</a></h2>
-            <div class="episode-time"><span>Sun, 03 Jul 2022 10:22:54 +0000</span> •
-                <span>9 minutes</span></div>
-            <p>
-                كيف تبني متجرك الالكتروني ؟؟ الأخطاء الشائعة في التجارة الالكترونية ؟؟ يلا نسمع ونستفيد
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/MjgzOQ/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">كيف تبدا في عالم التجارة الإلكترونية</a></h2>
-            <div class="episode-time"><span>Thu, 30 Jun 2022 08:58:42 +0000</span> •
-                <span>21 minutes</span></div>
-            <p>
-                ممكن تبدا المتجر الالكتروني من منتج واحد فقط وممكن تبدا متجر الكتروني من 30 منتج فقط وممكن تبدا متجر الكتروني بـ 100 منتج هذا كله يعتمد على أي نوع من أنواع التجارة الالكترونية وبتعمد أيضا على راس المال المخصص للمتجر سواء كان بالدروب شيبنج او بشراء المنتجات
-                وبيعها
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/MjczNA/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">حول مهارتك الى مصدر دخل حقيقي</a></h2>
-            <div class="episode-time"><span>Fri, 17 Jun 2022 11:00:00 +0000</span> •
-                <span>16 minutes</span></div>
-            <p>
-                كلا منا لديه مهاره ، في هذا البودكاست راح اوجهك على الطريق الصحيح لتحويل مهارتك لمصدر دخل إضافي
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/MjYyNg/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">اعلانات ترند سبب في نجاح واغلاق شركات</a></h2>
-            <div class="episode-time"><span>Sat, 11 Jun 2022 19:04:22 +0000</span> •
-                <span>13 minutes</span></div>
-            <p>
-                هذا الاعلان عمل نقله نوعية لمتاجر شركات فهذي المواضيع اللي لازم انت تركز عليها اكثر اثناء ما بتعمل اعلان العناصر المستخدمة مهمه جدا ————————- الاعلان هو المكسب الرئيسي الك على فكرة انت ممكن تكون عامل خطة تسويقية مش متكاملة والاعلان هو اللي ينجح لك هذي
-                الخطة
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/MjU4Ng/audio.mp3"></audio></div>
-        <div class="item">
-            <h2><a href="https://podeo.co" target="_blank">الحملة التسويقية الناجحة</a></h2>
-            <div class="episode-time"><span>Tue, 07 Jun 2022 11:27:18 +0000</span> •
-                <span>12 minutes</span></div>
-            <p>
-                الجمهور المتفاعل هو بداية نجاح اي حملة تسويقية ، اتكلم عن بعض الاسباب لفشل الحملات التسويقية يلا نستمتع …
-            </p><audio controls="true" preload="none" src="https://media.podeo.co/episodes/MjI4Mg/audio.mp3"></audio></div>
+        @endforeach
+        
+        
+      
     </div>
 </body>
 
